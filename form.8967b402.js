06a84d43d4bb045eb33d4d8475875b2218720eb6
Ivan3777 <103601460+Ivan3777@users.noreply.github.com>
2022-08-27 13:37:19 +0000

    Deploying to gh-pages from @ kyzym/55-bl-parcel@ecff3b883d1efe94a0bf383ec6fde98a953ab53b 🚀

--- form.8967b402.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".contact-form"),t={};e.addEventListener("input",(function(e){var n=e.target,o=n.name,r=n.value;t[o]=r;var a=JSON.stringify(t);console.log(a),localStorage.setItem("formKey",a)}))}();
-//# sourceMappingURL=form.8967b402.js.map
