e18134912976e56c8267246b726640f8f2766313
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2022-08-27 15:35:03 +0200

    input-2

--- src/js/form.js
+++ src/js/form.js
@@ -1,34 +1,43 @@
+import storageAPI from './storage';
+
+console.log(storageAPI);
 const form = document.querySelector('.contact-form');
 const formData = {};
 initPage();
+
 function handleInput(event) {
   const { name, value } = event.target;
-  formData[name] = value;
 
-  try {
-    const formDataJSON = JSON.stringify(formData);
-    console.log(formDataJSON);
-    localStorage.setItem('formKey', formDataJSON);
-  } catch (error) {
-    console.log(error.message);
-  }
+  let savedData = storageAPI.load('formKey');
+  savedData = savedData ? savedData : {};
+  savedData[name] = value;
+  storageAPI.save('formKey', savedData);
+  // try {
+  //   let savedData = localStorage.getItem('formKey');
+  //   if (savedData) {
+  //     savedData = JSON.parse(savedData);
+  //   } else {
+  //     savedData = {};
+  //   }
+  //   const formDataJSON = JSON.stringify(savedData);
+  //   console.log(formDataJSON);
+  //   localStorage.setItem('formKey', formDataJSON);
+  // } catch (error) {
+  //   console.log(error.message);
+  // }
 }
 
 form.addEventListener('input', handleInput);
 
-function initPage(arguments) {
-  try {
-    const savedData = localStorage.getItem('formKey');
+function initPage() {
+  const savedData = storageAPI.load('formKey');
 
-    if (!savedData) {
-      return;
-    }
-    const savedDataObj = JSON.parse(savedData);
-    console.log(savedDataObj);
-    Object.entries(savedDataObj).forEach(([name, value]) => {
-      form.elements[name].value = value;
-    });
-  } catch (error) {
-    console.log(error.message);
+  if (!savedData) {
+    return;
   }
+  const savedDataObj = JSON.parse(savedData);
+  console.log(savedDataObj);
+  Object.entries(savedDataObj).forEach(([name, value]) => {
+    form.elements[name].value = value;
+  });
 }
