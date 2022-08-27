97cdaa736037d7877a98c7b2f0ee089117260e8f
Aleksey Knigin <alexyo2626@gmail.com>
2022-08-27 16:13:11 +0300

    init-page

--- src/js/form.js
+++ src/js/form.js
@@ -1,13 +1,34 @@
 const form = document.querySelector('.contact-form');
 const formData = {};
-
+initPage();
 function handleInput(event) {
   const { name, value } = event.target;
   formData[name] = value;
 
-  const formDataJSON = JSON.stringify(formData);
-  console.log(formDataJSON);
-  localStorage.setItem('formKey', formDataJSON);
+  try {
+    const formDataJSON = JSON.stringify(formData);
+    console.log(formDataJSON);
+    localStorage.setItem('formKey', formDataJSON);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 form.addEventListener('input', handleInput);
+
+function initPage(arguments) {
+  try {
+    const savedData = localStorage.getItem('formKey');
+
+    if (!savedData) {
+      return;
+    }
+    const savedDataObj = JSON.parse(savedData);
+    console.log(savedDataObj);
+    Object.entries(savedDataObj).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
