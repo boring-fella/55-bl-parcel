268623ade027f33870dc5edf4a7ead1d19d575b3
Ivan Vodopianov <vis3010@gmail.com>
2022-08-27 16:52:06 +0300

    submit

--- src/js/form.js
+++ src/js/form.js
@@ -1,8 +1,6 @@
 import storageAPI from './storage';
 
-console.log(storageAPI);
 const form = document.querySelector('.contact-form');
-const formData = {};
 initPage();
 
 function handleInput(event) {
@@ -12,26 +10,13 @@ function handleInput(event) {
   savedData = savedData ? savedData : {};
   savedData[name] = value;
   storageAPI.save('formKey', savedData);
-  // try {
-  //   let savedData = localStorage.getItem('formKey');
-  //   if (savedData) {
-  //     savedData = JSON.parse(savedData);
-  //   } else {
-  //     savedData = {};
-  //   }
-  //   const formDataJSON = JSON.stringify(savedData);
-  //   console.log(formDataJSON);
-  //   localStorage.setItem('formKey', formDataJSON);
-  // } catch (error) {
-  //   console.log(error.message);
-  // }
+
 }
 
 form.addEventListener('input', handleInput);
 
 function initPage() {
   const savedData = storageAPI.load('formKey');
-console.log(savedData);
   if (!savedData) {
     return;
   }
